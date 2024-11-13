c2f623a96afa63216a551285dce8ad04e6c1b4c1
Ubuntu <ubuntu@ip-172-31-24-108.ec2.internal>
2024-11-13 07:23:25 +0000

    init

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
+
 // Load environment variables from .env file
-<<<<<<< HEAD
-requinpmre('dotenv').config();
-=======
 require('dotenv').config();
->>>>>>> 81bc8fd (Initial commit)
 
 const express = require('express');
 const app = express();
 
 // Use environment variables
 const PORT = process.env.PORT || 3000;
-const MESSAGE = process.env.MESSAGE || "Hello World!";
+const MESSAGE = process.env.MESSAGE || "Hello Worldd!";
 
 // Basic route
 app.get('/', (req, res) => {
@@ -20,8 +17,4 @@ app.get('/', (req, res) => {
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> 81bc8fd (Initial commit)
