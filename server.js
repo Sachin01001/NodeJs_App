8b0090ba1ee53c61c033afd94c73376e1d36826a
Sachin01001 <sbobade535@gmail.com>
2024-11-14 16:02:09 +0530

    Your commit message

--- server.js
+++ server.js
@@ -1,32 +1,13 @@
-// // Load environment variables from .env file
-// require('dotenv').config();
 
-// const express = require('express');
-// const app = express();
-
-// // Use environment variables
-// const PORT = process.env.PORT || 3000;
-// const MESSAGE = process.env.MESSAGE || "Hello World!";
-
-// // Basic route
-// app.get('/', (req, res) => {
-//     res.send(MESSAGE);
-// });
-
-// // Start the server
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-// });
-// Load environment variables from .env file
 require('dotenv').config()
 const express = require('express');
 const app = express();
 
 // Use environment variables
-const PORT = process.env.PORT || 3000;
-const MESSAGE = process.env.MESSAGE || "Hello Worlddd!";
+const PORT = process.env.PORT || 4000;
+const MESSAGE = process.env.MESSAGE || "Hello World!";
 
-// Basic route
+// Basic routegit
 app.get('/', (req, res) => {
     res.send(MESSAGE);
 });
